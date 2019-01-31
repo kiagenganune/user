4b3fcb4263532980919cff17171cc9c1a4f9982f
kiagenganune <47199964+kiagenganune@users.noreply.github.com>
2019-01-31 13:09:45 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,56 @@
+<?php
+echo $ijo."[•••••] sekilas info [••••••]".$t;
+echo $putih."[-] Date : Selasa 31-Januari-2019".$t;
+echo $kuning."[-] 01 akun W&E hari ini Telah di konfirmasi sebagai member".$t;
+echo $kuning."[-] Di Perbarui Pada Jam : 00.32 WIT".$t;
+echo $kuning."[-] Mohon Maaf Admin sedang di luar kota".$t;
+echo $kuning."[-] Jadi sinyal down, Untuk pendfaftaran Member W&E jadi terhambat mohon maaf".$t;
+$stat_ads=false;
+$sungging = $putih.'[*] '.$ijo.'subcribe youtube Per-mana '.$putih.'[-] code: '.$ijo.'J62347';
+$ads= $putih.'[*] '.$ijo.'subcribe youtube Per-mana '.$putih.'[-] code: '.$ijo.'J62347';
+$msgtoto = "[!] Subscribe Youtube Per-mana".$t.$ijo."[•••••] sekilas info [••••••]
+hallo guys apa kabar
+yang masih setia menunggu update script terbaru toto
+sedikit sy jelaskan perjalanan kita
+>> toto berganti menjadi TTNews 
+>> TTNews berganti menjadi watch news
+>> watch news berganti menjadi cworld
+nah saat ini cworld masih dalam tahap beta tes
+untuk yg blm punya apk cworld bisa di download
+di sini >> https://sfile.mobi/8ZPF2b181Ut
+atau kunjungi playstore
+di sini >> http://news.masjmzs.com/index/openredpacket?unique=e1d4bac1b720a2af7c0acf3d8f81617c&d=31&lng=in
+ 
+dan disini >> 
+https://play.google.com/store/apps/details?id=com.news.newsyayd
+setelah menginstall aplikasi terbaru cworld
+silahkan login menggunakan akun favorit kalian
+dan ketika di minta pilihan
+ >> new user atau old user <<
+silahkan pilih new user untuk menghilangkan jejak kita
+setelah login berhasil masuk ke tab profil
+di kanan bawah dan pergi untuk masukan kode undangan
+silahkan memasukan kode >> A15280 <<
+skrip akan sy update setelah close beta tes
+atau setelah cworld di rillis
+jika sebelum rillis ada 200+ user
+menggunakan kode saya maka skrip akan sy buat free
+tapi jika kurang dari itu
+maka system validasi akan berlaku seperti watch earn
+demikian sekilas info$putih
+msg_by:$ijo adidoank";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["Imam Almasyah Risanzani",];
+$ydblock = ["Imam Almasyah Risanzani",];
+$we_stat = true;
+$msgwe = "[•] Subscribe itu gratis";
+$msgveeu = "[!] Subscribe Free";
+$data= [
+//info
+//rf
+'Jijo',
+       ];
+$toto = ['tes',];
+$ydnews = ['tes','ting',];
